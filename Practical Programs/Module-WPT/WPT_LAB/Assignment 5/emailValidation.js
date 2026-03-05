8381370730ab5fee7910ef64e2cc101fd8bc3ca1
komalghodke <komalghodke99@gmail.com>
2026-03-05 19:43:59 +0530

    Added WPT 5th Assignment

---
+++ Practical Programs/Module-WPT/WPT_LAB/Assignment 5/emailValidation.js
@@ -0,0 +1,17 @@
+function validate() {
+    var data = document.getElementById("a1").value;
+    //alert(data);
+    var i = data.indexOf('@');
+    if (i > 0) {
+        var j = data.lastIndexOf('.');
+        if (j > i) {
+            console.log("Valid Format");
+        }
+        else {
+            console.log("Invalid Format ");
+        }
+    }
+    else {
+        console.log("Invalid Format ");
+    }
+}
