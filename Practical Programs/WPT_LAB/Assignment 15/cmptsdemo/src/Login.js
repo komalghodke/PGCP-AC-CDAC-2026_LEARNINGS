849327ced93e12c9c5a298e814dda6913c91305f
komalghodke <komalghodke99@gmail.com>
2026-03-19 20:56:07 +0530

    WPT assignment

--- Practical Programs/WPT_LAB/Assignment 15/cmptsdemo/src/Login.js
+++ Practical Programs/WPT_LAB/Assignment 15/cmptsdemo/src/Login.js
@@ -3,33 +3,49 @@ import { Component } from "react";
 class Login extends Component{
 
     constructor(props) {
-        super(props);
-        this.state ={msg: "default"}
+        super(props); //component Instance
+        this.state = {
+            username: "",
+            password: "", 
+            msg: "Miss/Mrs"
+        }
         this.btnHandler = this.btnHandler.bind(this);
         this.txtHandler = this.txtHandler.bind(this);
-    }
-
-    btnHandler() {
-        console.log("Ok for Login!")
+        this.passHandler = this.passHandler.bind(this);
     }
 
     txtHandler(e) {
-        console.log("Target name: " +e.target.name);
-        console.log("Username: " +e.target.value);
-        this.setState({msg: e.target.value});
+        console.log("Target name: " + e.target.name);
+        console.log("Username: " + e.target.value);
+        this.setState({username: e.target.value}); // update username
     }
 
-    
+    passHandler(e) {
+        console.log("Target name: " + e.target.name);
+        console.log("Password: " + e.target.value);
+        this.setState({password: e.target.value}); // update password
+    }
+
+    btnHandler(e) {
+        console.log("Inside btnHandler!");
+        const {username, password} = this.state;
+        if(username === "iet" && password === "iet"){
+            this.setState({msg: `Hello ${username}`});
+        } else {
+            this.setState({msg: "Invalid credentials"});
+        }
+    }
+
     render() {
         return <>
         <pre> 2) Create LoginForm using class component accept username and password and display hello username on same page. if username = "iet" and password = "iet" </pre>
         <h2> Login Form </h2>
         <b>
         Username <input type="text" name="uName" id="uName" onBlur={this.txtHandler}/> <br/>
-        Password <input type="password" name="pass" id="pass"/>
+        Password <input type="password" name="pass" id="pass" onBlur={this.passHandler}/>
         <br/>
         <button value={"LOGIN"} onClick={this.btnHandler}> LOGIN </button>
-        <p> Hello, {this.state.msg}!</p>
+        <p> {this.state.msg}</p>
         </b>
         </>
     }
