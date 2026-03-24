d8fc77f06a2476becb2bed0238086a28d79c8c3a
komalghodke <komalghodke99@gmail.com>
2026-03-24 12:21:01 +0530

    Added completed WPT Assignment 18

--- Practical Programs/WPT_LAB/Assignment 18/react-demo/src/App.js
+++ Practical Programs/WPT_LAB/Assignment 18/react-demo/src/App.js
@@ -1,18 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import DemoCounter from './DemoCounter';
-import MyFunction from './MyFunction';
 import DemoError from './DemoError';
 import BgColorDemo from './BgColorDemo';
+import ErrorHandler from "./ErrorHandler";
+import MyFunction from "./MyFunction";
 
 function App() {
   return (
-    <div className="App">
-    <DemoCounter></DemoCounter>
-    {/* <MyFunction></MyFunction> */}
-    <DemoError></DemoError>
-    <BgColorDemo></BgColorDemo>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav style={{ backgroundColor: "#eee", padding: "10px" }}>
+          <Link to="/"> Counter</Link> | 
+          <Link to="/error"> Error Demo</Link> | 
+          <Link to="/color"> Background Demo</Link> |
+          <Link to="/runtime"> Runtime Error</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<DemoCounter />} />
+          <Route path="/error" element={<DemoError />} />
+          <Route path="/color" element={<BgColorDemo />} />
+          <Route path="/runtime" element = {
+            <ErrorHandler>
+              <MyFunction />
+            </ErrorHandler> }/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
