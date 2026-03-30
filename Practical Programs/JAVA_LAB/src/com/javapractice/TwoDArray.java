f5bf4fd64abba04bf6667f9125815907399f04a1
komalghodke <komalghodke99@gmail.com>
2026-03-30 13:23:04 +0530

    JAVA Practice 28th March

--- Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArray.java
+++ Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArray.java
@@ -5,26 +5,138 @@ import java.util.Scanner;
 public class TwoDArray {
 
 	public static void getMatrix(int[][] arr1) {
-		System.out.print("Please enter the Elements of First 2D Array: ");
 		Scanner sc = new Scanner(System.in);
 		for(int i=0; i<arr1.length; i++) {
-			for(int j=0; j<arr1.length; j++) {
+			for(int j=0; j<arr1[i].length; j++) {
 				arr1[i][j] = sc.nextInt();
 			}
 		}
 	}
 	
 	public static void displayMatrix(int[][] arr1) {
-		System.out.println("First Matrix: ");
-		Scanner sc = new Scanner(System.in);
 		for(int i=0; i<arr1.length; i++) {
-			for(int j=0; j<arr1.length; j++) {
+			for(int j=0; j<arr1[i].length; j++) {
 				System.out.print(arr1[i][j] + "\t");
 			}
 			System.out.println();
 		}
 	}
 	
+	public static int findMax(int[][] arr1) {
+		int max = arr1[0][0];
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1[i].length; j++) {
+				if(max < arr1[i][j]) {
+					max=arr1[i][j];
+					}
+			}
+		}
+		return max;
+	}
 	
+	public static int findMin(int[][] arr1) {
+		Scanner sc = new Scanner(System.in);
+		int min = arr1[0][0];
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1[i].length; j++) {
+				if(min > arr1[i][j]) {
+					min=arr1[i][j];
+					}
+			}
+		}
+		return min;
+	}
 
+	public static int[] findSumRowWise(int[][] arr1) {
+		int[] sumR = new int[arr1.length];
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1[i].length; j++) {
+				sumR[i] += arr1[i][j];
+			}
+		}
+		return sumR;
+	}
+
+	public static int[] findSumColWise(int[][] arr) {
+		int[] sumC = new int[arr[0].length];
+		for(int i=0; i<arr[0].length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				sumC[i] += arr[j][i];
+			}
+		}
+		return sumC;
+	}
+
+	public static int[] findMaxRowWise(int[][] arr) {
+		int[] maxR = new int[arr.length];
+		for(int i=0; i<arr.length; i++) {
+			maxR[i]=arr[i][0];
+			for(int j=0; j<arr[i].length; j++) {
+				if(maxR[i]<arr[i][j]) {
+					maxR[i] = arr[i][j];
+				}
+			}
+		}
+		return maxR;
+	}
+
+	public static int[] findMaxColWise(int[][] arr) {
+		int[] maxC = new int[arr[0].length];
+		for(int i=0; i<arr[0].length; i++) {
+			maxC[i]=arr[0][i];
+			for(int j=0; j<arr.length; j++) {
+				if(maxC[i]<arr[i][j]) {
+					maxC[i] = arr[j][i];
+				}
+			}
+		}
+		return maxC;
+	}
+
+	public static int[] findMinRowWise(int[][] arr) {
+		int[] minR = new int[arr.length];
+		for(int i=0; i<arr.length; i++) {
+			minR[i]=arr[i][0];
+			for(int j=0; j<arr[i].length; j++) {
+				if(minR[i]>arr[i][j]) {
+					minR[i] = arr[i][j];
+				}
+			}
+		}
+		return minR;
+	}
+	
+	public static int[] findMinColWise(int[][] arr) {
+		int[] minC = new int[arr[0].length];
+		for(int i=0; i<arr[0].length; i++) {
+			minC[i]=arr[0][i];
+			for(int j=0; j<arr.length; j++) {
+				if(minC[i]>arr[i][j]) {
+					minC[i] = arr[j][i];
+				}
+			}
+		}
+		return minC;
+	}
+
+	public static int[][] addition(int[][] arr1, int[][] arr2) {
+		int[][] add = new int[arr1.length][arr2.length];
+		for(int i=0; i<arr1[0].length; i++) {
+			for(int j=0; j<arr1.length; j++) {
+				add[i][j] = arr1[i][j] + arr2[i][j];
+				}
+		}
+		return add;
+	}
+
+	public static int[][] substract(int[][] arr1, int[][] arr2) {
+		int[][] sub = new int[arr1.length][arr2.length];
+		for(int i=0; i<arr1[0].length; i++) {
+			for(int j=0; j<arr1.length; j++) {
+				sub[i][j] = arr1[i][j] - arr2[i][j];
+				}
+		}
+		return sub;
+	}
+	
 }
--- Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArrayTest.java
+++ Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArrayTest.java
@@ -1,15 +1,72 @@
 package com.javapractice;
 
-import java.util.Scanner;
-
 public class TwoDArrayTest {
 
 	public static void main(String[] args) {
 
 		int[][] arr1 = new int[3][3];
+		System.out.print("Please enter the Elements of First 2D Array: ");
 		TwoDArray.getMatrix(arr1);
+		System.out.println("Matrix 1:");
 		TwoDArray.displayMatrix(arr1);
 		
+		int[][] arr2 = new int[3][3];
+		System.out.print("Please enter the Elements of Second 2D Array: ");
+		TwoDArray.getMatrix(arr2);
+		System.out.println("Matrix 2:");
+		TwoDArray.displayMatrix(arr2);
+		
+		int[][] res = TwoDArray.addition(arr1, arr2);		
+		System.out.println("Addition Matrix of Matrix 1 and Matrix 2 is: ");
+		TwoDArray.displayMatrix(res);
+		
+		int[][] resSub = TwoDArray.substract(arr1, arr2);		
+		System.out.println("Substraction Matrix of Matrix 1 and Matrix 2 is: ");
+		TwoDArray.displayMatrix(resSub);
+		
+		int max = TwoDArray.findMax(arr1);
+		System.out.println("\nMaximum element from Matrix 1: " + max);
+		
+		int min = TwoDArray.findMin(arr1);
+		System.out.println("Minimum element from Matrix 1: " + min);
+		
+		int[] sumArr =  TwoDArray.findSumRowWise(arr1);
+		for(int i=0; i<sumArr.length; i++) {
+			System.out.println("Sum of Row Number " +i+ " of Matrix is: " + sumArr[i]);
+		}
+		
+		int[] colSum =  TwoDArray.findSumColWise(arr1);
+		for(int i=0; i<colSum.length; i++) {
+			System.out.println("Sum of Column Number " +i+ " of Matrix is: " + colSum[i]);
+		}
+		
+		System.out.println();
+		
+		int[] maxRow =  TwoDArray.findMaxRowWise(arr1);
+		for(int i=0; i<maxRow.length; i++) {
+			System.out.println("Max element of row Number " +i+ " of Matrix is: " + maxRow[i]);
+		}
+		
+		System.out.println();
+		
+		int[] minRow =  TwoDArray.findMinRowWise(arr1);
+		for(int i=0; i<minRow.length; i++) {
+			System.out.println("Minimum element of row Number " +i+ " of Matrix is: " + minRow[i]);
+		}
+		
+		System.out.println();
+		
+		int[] maxCol =  TwoDArray.findMaxColWise(arr1);
+		for(int i=0; i<maxCol.length; i++) {
+			System.out.println("Max element of Column Number " +i+ " of Matrix is: " + maxCol[i]);
+		}
+
+		System.out.println();
+		
+		int[] minCol =  TwoDArray.findMinColWise(arr1);
+		for(int i=0; i<minCol.length; i++) {
+			System.out.println("Minimum element of Column Number " +i+ " of Matrix is: " + minCol[i]);
+		}
 	}
 
 }
