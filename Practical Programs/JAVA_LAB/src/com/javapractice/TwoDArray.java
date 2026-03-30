66b716f586458836f3949ec7a649aff35c1721c9
komalghodke <komalghodke99@gmail.com>
2026-03-30 11:32:01 +0530

    JAVA - 28th March

--- Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArray.java
+++ Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArray.java
@@ -1,5 +1,30 @@
 package com.javapractice;
 
+import java.util.Scanner;
+
 public class TwoDArray {
+
+	public static void getMatrix(int[][] arr1) {
+		System.out.print("Please enter the Elements of First 2D Array: ");
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1.length; j++) {
+				arr1[i][j] = sc.nextInt();
+			}
+		}
+	}
+	
+	public static void displayMatrix(int[][] arr1) {
+		System.out.println("First Matrix: ");
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1.length; j++) {
+				System.out.print(arr1[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	
 
 }
--- Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArrayTest.java
+++ Practical Programs/JAVA_LAB/src/com/javapractice/TwoDArrayTest.java
@@ -1,9 +1,15 @@
 package com.javapractice;
 
+import java.util.Scanner;
+
 public class TwoDArrayTest {
 
 	public static void main(String[] args) {
 
+		int[][] arr1 = new int[3][3];
+		TwoDArray.getMatrix(arr1);
+		TwoDArray.displayMatrix(arr1);
+		
 	}
 
 }
