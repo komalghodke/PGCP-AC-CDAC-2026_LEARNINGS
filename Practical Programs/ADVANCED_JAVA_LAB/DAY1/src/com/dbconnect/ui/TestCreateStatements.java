43e2803f978099a5c4b0d7ae07db79b32ad20bc1
komalghodke <komalghodke99@gmail.com>
2026-05-19 16:31:02 +0530

    Advanced JAVA - DB Connection done, implemented create statement, prepared statement

--- Practical Programs/ADVANCED_JAVA_LAB/DAY1/src/com/dbconnect/ui/TestCreateStatements.java
+++ Practical Programs/ADVANCED_JAVA_LAB/DAY1/src/com/dbconnect/ui/TestCreateStatements.java
@@ -2,10 +2,11 @@ package com.dbconnect.ui;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class TestConnection {
-
+public class TestCreateStatements {
 	public static void main(String[] args) {
 		try {
 			// STEP 1 : Registered Driver
@@ -20,6 +21,32 @@ public class TestConnection {
 			} else {
 				System.out.println("Connection not Established!");
 			}
+
+			// STEP 3 : Create Statement
+			Statement st = conn.createStatement();
+
+			// STEP 4 : Execute the Statement
+			ResultSet rs = st.executeQuery("select * from dummy");
+
+			// STEP 5 : Navigate through Resultset
+			while (rs.next()) {
+				System.out.println("ID: " + rs.getInt(1));
+				System.out.println("Name: " + rs.getString(2));
+			}
+
+			// To insert data
+			int id = 1;
+			String name = "second";
+			String query = "insert into dummy values(" + id + ",'" + name + "')";
+			System.out.println(query);
+
+			int n = st.executeUpdate(query);
+			if (n > 0) {
+				System.out.println("Insertion Done!");
+			} else {
+				System.out.println("Insertion not Done");
+			}
+
 			conn.close();
 		} catch (SQLException e) {
 			System.out.println("Connection couldn't be Established!");
---
+++ Practical Programs/ADVANCED_JAVA_LAB/DAY1/src/com/dbconnect/ui/TestPreparedStatements.java
@@ -0,0 +1,60 @@
+package com.dbconnect.ui;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestPreparedStatements {
+
+	public static void main(String[] args) {
+		try {
+			// STEP 1 : Registered Driver
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+			// STEP 2 : Create Connection
+			String url = "jdbc:mysql://localhost:3306/dummy?useSSL=false";
+			// &allowPublicKeyRetrieval=true
+			Connection conn = DriverManager.getConnection(url, "root", "root");
+			if (conn != null) {
+				System.out.println("Connection Established!");
+			} else {
+				System.out.println("Connection not Established!");
+			}
+
+			// STEP 3 : PreparedStatement
+			PreparedStatement pst = conn.prepareStatement("select * from dummy1");
+
+			// STEP 4 : Execute the Statement
+			ResultSet rs = pst.executeQuery();
+
+			// STEP 5 : Navigate through Resultset
+			while (rs.next()) {
+				System.out.println("ID: " + rs.getInt(1));
+				System.out.println("Name: " + rs.getString(2));
+			}
+
+			// To insert data
+			int id = 2;
+			String name = "Second";
+			PreparedStatement pst1 = conn.prepareStatement("insert into dummy1 values(?,?)");
+			pst1.setInt(1, id);
+			pst1.setString(2, name);
+
+			int n = pst1.executeUpdate();
+			if (n > 0) {
+				System.out.println("insertion done");
+			} else {
+				System.out.println("insertion not done");
+			}
+			
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("Connection couldn't be Established!");
+			e.printStackTrace();
+		}
+	}
+
+}
