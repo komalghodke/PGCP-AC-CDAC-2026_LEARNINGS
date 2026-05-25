abc0ba09d656aebda6d5d1eb5509495b44f418ed
komalghodke <komalghodke99@gmail.com>
2026-05-25 14:09:25 +0530

    Added Selenium demo project 1

---
+++ Practical Programs/Selenium_LAB/src/main/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
